b2ebc4401cd68efd5e8fbe81c6e45cf261284579
Evan Martin <evan.martin@shaw.ca>
2017-07-24 11:25:09 -0700

    made functions

--- crop-screencap.py
+++ crop-screencap.py
@@ -38,66 +38,25 @@ def getImageContour(contours):
 	return image_contour
 
 def saveCroppedImage(im, image_contour):
-	cv2.drawContours(im, [screen_contour], -1, (255,255,0), 3)
+	cv2.drawContours(im, [image_contour], -1, (255,255,0), 3)
 	cv2.imwrite('im_with_contour.jpg',im)
 
-	y0 = min(screen_contour[0][0][1],screen_contour[1][0][1],screen_contour[2][0][1],screen_contour[3][0][1])
-	y1 = max(screen_contour[0][0][1],screen_contour[1][0][1],screen_contour[2][0][1],screen_contour[3][0][1])
+	y0 = min(image_contour[0][0][1],image_contour[1][0][1],image_contour[2][0][1],image_contour[3][0][1])
+	y1 = max(image_contour[0][0][1],image_contour[1][0][1],image_contour[2][0][1],image_contour[3][0][1])
 
-	x0 = min(screen_contour[0][0][0],screen_contour[1][0][0],screen_contour[2][0][0],screen_contour[3][0][0])
-	x1 = max(screen_contour[0][0][0],screen_contour[1][0][0],screen_contour[2][0][0],screen_contour[3][0][0])
+	x0 = min(image_contour[0][0][0],image_contour[1][0][0],image_contour[2][0][0],image_contour[3][0][0])
+	x1 = max(image_contour[0][0][0],image_contour[1][0][0],image_contour[2][0][0],image_contour[3][0][0])
 	print y0,y1,x0,x1
 	im_cropped = im[y0:y1, x0:x1]
 	cv2.imwrite('im_cropped.jpg',im_cropped)
 
-imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-imgray = cv2.bilateralFilter(imgray, 11, 17, 17)
 
-#1
-# edged = cv2.Canny(imgray, 30, 200)
-#2
-ret, thresh = cv2.threshold(imgray, 240, 240, 0)
+def main():
+	im = 'img0.jpg'
+	im = cv2.imread(im)
 
-#1
-# im2, contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	contours = getContours(im)
+	image_contour = getImageContour(contours)
+	saveCroppedImage(im, image_contour)
 
-#2
-im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-
-screen_contour = None
-
-count = 0
-for contour in contours:
-	peri = cv2.arcLength(contour, True)
-	approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
-
-	if len(approx) == 4:
-		screen_contour = approx
-		count = count + 1
-		if count > 1:
-			print screen_contour
-			break
-
-# show the screen_contour on the image
-cv2.drawContours(im, [screen_contour], -1, (255,255,0), 3)
-
-
-cv2.imwrite('im_with_contour.jpg',im)
-
-y0 = min(screen_contour[0][0][1],screen_contour[1][0][1],screen_contour[2][0][1],screen_contour[3][0][1])
-y1 = max(screen_contour[0][0][1],screen_contour[1][0][1],screen_contour[2][0][1],screen_contour[3][0][1])
-
-x0 = min(screen_contour[0][0][0],screen_contour[1][0][0],screen_contour[2][0][0],screen_contour[3][0][0])
-x1 = max(screen_contour[0][0][0],screen_contour[1][0][0],screen_contour[2][0][0],screen_contour[3][0][0])
-
-print y0,y1,x0,x1
-
-im_cropped = im[y0:y1, x0:x1]
-
-cv2.imwrite('im_cropped.jpg',im_cropped)
-
-
-# cv2.imshow("cropped", imgray)
-# cv2.waitKey(0)
+main()
